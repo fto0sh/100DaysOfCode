382a015b7bec93d06310ab87c7558e7d6069d5a7
fto0sh <53145854+fto0sh@users.noreply.github.com>
2019-08-21 16:40:08 +0300

    Add files via upload

---
+++ daay2.py
@@ -0,0 +1,11 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> """
+This is a commit
+written in
+more than just one line
+"""
+'\nThis is a commit\nwritten in\nmore than just one line\n'
+>>> print("hello world")
+hello world
+>>> 
