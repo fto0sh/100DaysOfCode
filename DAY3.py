4f219ef8ca7a378946169396b49ab9bde0ac461d
fto0sh <fatimah.s.9@hotmail.com>
2019-08-22 14:21:10 +0300

    123

---
+++ DAY3.py
@@ -0,0 +1,41 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x =5
+>>> y ="John"
+>>> print (x)
+5
+>>> print(y)
+John
+>>> 
+x = 4 #x is of type int
+>>> x = "Sally" # x is now of type str
+>>> print (x)
+Sally
+>>> x ="John"
+>>> print (x)
+John
+>>> x = 'john'
+>>> print (x)
+john
+>>> x,y,z ="Orange", "Banana", "Cherry"
+>>> print(x) print(y) print(z)
+SyntaxError: invalid syntax
+>>> print(x) , print(y), print(z)
+Orange
+Banana
+Cherry
+(None, None, None)
+>>> x= "awesome"
+>>> print("Python is" + x)
+Python isawesome
+>>> x="Python is "
+>>> y="fto0sh"
+>>> z=x + y
+>>> print(z)
+Python is fto0sh
+>>> x=5
+>>> y=10
+>>> z=x+y
+>>> print(z)
+15
+>>> 
