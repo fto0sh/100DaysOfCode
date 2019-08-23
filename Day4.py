386cfc1fb2e007cb8a2f71db59f629fa71d740db
fto0sh <fatimah.s.9@hotmail.com>
2019-08-23 19:27:55 +0300

    day 4

---
+++ Day4.py
@@ -0,0 +1,84 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x =1 #int
+>>> y=2.8 #float
+>>> z=1j #complex
+>>> print (type(x)))
+SyntaxError: invalid syntax
+>>> print (type (x) )
+<class 'int'>
+>>> print(type (y) )
+<class 'float'>
+>>> print(type (z) )
+<class 'complex'>
+>>> x= 1
+>>> y= 35656544444
+>>> z=-325522
+>>> print (type (x) )
+<class 'int'>
+>>> print (type (y) 0
+       
+SyntaxError: invalid syntax
+>>> print (print (y) )
+35656544444
+None
+>>> print (type (z) )
+<class 'int'>
+>>> x=1.10
+>>> y=1.0
+>>> z= -35.59
+>>> print (type(x) )
+<class 'float'>
+>>> print (type(y) )
+<class 'float'>
+>>> print (type(z) )
+<class 'float'>
+>>> 
+x=35e3
+>>> y=12e4
+>>> z=-87.7e100
+>>> print(type(x) )
+<class 'float'>
+>>> print (type(y) )
+<class 'float'>
+>>> print (type (z) )
+<class 'float'>
+>>> x= 3+5j
+>>> y=5j
+>>> z= -5j
+>>> print (type(x) )
+<class 'complex'>
+>>> print (type(y) )
+<class 'complex'>
+>>> print (type(z) )
+<class 'complex'>
+>>> x = 1 #int
+>>> y =2.8 #float
+>>> z= 1j #complex
+>>> #convert form float int to float:
+>>> a=float(x)
+>>> #convert form float to int
+>>> b=int (y)
+>>> #convert from int to complex:
+>>> c=complex(x)
+>>> print(a)
+1.0
+>>> print(b)
+2
+>>> print(c)
+(1+0j)
+>>> print(type(a))
+<class 'float'>
+>>> print(type(b))
+<class 'int'>
+>>> print(type(c))
+<class 'complex'>
+>>> import random
+>>> print(random.randrange(1,10))
+2
+>>>  print(random.randrange(1,10))
+ 
+SyntaxError: unexpected indent
+>>> print(random.randrange(1,10))
+4
+>>> 
