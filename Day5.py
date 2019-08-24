dfb6b051c9a293acff5119baf3d9d079e7e57e15
fto0sh <fatimah.s.9@hotmail.com>
2019-08-24 14:33:01 +0300

    day5

---
+++ Day5.py
@@ -0,0 +1,7 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x, y, z = "apple", "orange", "limon"
+>>> basket=x+y+z
+>>> print(basket)
+appleorangelimon
+>>> 
