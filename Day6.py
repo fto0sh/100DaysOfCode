a8c3da21f5e9baa91b53d7f753bfe1e268780cf9
fto0sh <fatimah.s.9@hotmail.com>
2019-08-26 13:05:31 +0300

    day 6

---
+++ Day6.py
@@ -0,0 +1,25 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = int(1) # x will be 1
+>>> y = int(2.8) #y will b 2
+>>> z = int("3") # z will be 3
+>>> print(x ,y ,z)
+1 2 3
+>>> x = float(1) #x will be 1.0
+>>> y = float(2.8) # y will be 2.8
+>>> z = float("3") #z will be 3
+>>> print(x ,y ,z)
+1.0 2.8 3.0
+>>> x = str("sl")
+>>> y= str(2)
+>>> z= str(3.0)
+>>> print(x ,y ,z)
+sl 2 3.0
+>>> print(x)
+sl
+>>> print(y)
+2
+>>> y = str(2)
+>>> print(y)
+2
+>>> 
---
+++ Day7.py
@@ -0,0 +1,33 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> print('hello')
+hello
+>>> a="hello"
+>>> print(a)
+hello
+>>> a = """Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting."""
+>>> print(a)
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+>>> a="hello ,word"
+>>> print(a[1])
+e
+>>> b="hello ,word"
+>>> print(b[2:5])
+llo
+>>> 
