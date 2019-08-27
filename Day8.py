4aabf92181d2f14a9adb9a0d8a851205c7863137
fto0sh <fatimah.s.9@hotmail.com>
2019-08-27 16:44:34 +0300

    day  8

---
+++ Day8.py
@@ -0,0 +1,21 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = " Hello, World! "
+>>> print(a.strip()) # returns "Hello, World!"
+Hello, World!
+>>> a = "Hello, World!"
+>>> print(len(a))
+13
+>>> a = "Hello, World!"
+>>> print(a.lower())
+hello, world!
+>>> a = "Hello, World!"
+>>> print(a.upper())
+HELLO, WORLD!
+>>> a = "Hello, World!"
+>>> print(a.replace("H", "J"))
+Jello, World!
+>>> a = "Hello, World!"
+>>> print(a.split(",")) # returns ['Hello', ' World!']
+['Hello', ' World!']
+>>> 
