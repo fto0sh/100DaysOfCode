b0bc0d9639febc9dab59742ebe0896d19830d8dd
fto0sh <fatimah.s.9@hotmail.com>
2019-08-30 17:56:21 +0300

    day  12

---
+++ Day12.py
@@ -0,0 +1,8 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Q=5
+>>> QQ=7
+>>> myorder="Please ,Iwant{} sandwishes and {} donutes"
+>>> print(myorder.replace("I" ,"we").replace("a","A").format(Q,QQ))
+PleAse ,wewAnt5 sAndwishes And 7 donutes
+>>> 
