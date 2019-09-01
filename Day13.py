135d90c892343cc0037d4d9501c9d85749194ff8
fto0sh <fatimah.s.9@hotmail.com>
2019-09-01 19:37:10 +0300

    day  13

---
+++ Day13.py
@@ -0,0 +1,34 @@
+number =[1,2,3,4,5,6]
+print(number)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+num=[float]*4
+num[0]=12.3
+num[1]=11.3
+num[2]=22.3
+num[3]=4.4
+print(num)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+
+list = [1, 2, 3,5,43,2]
+for x in list:
+  print(x)
+  
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
