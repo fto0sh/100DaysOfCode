040bd9b2850635fd3e7cdb192b2377602f366981
fto0sh <fatimah.s.9@hotmail.com>
2019-09-11 17:14:18 +0300

    day 20& 21

---
+++ day20.py
@@ -0,0 +1,23 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.update(["orange", "mango", "grapes"])
+
+print(thisset)
