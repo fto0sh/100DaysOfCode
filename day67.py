03665a50fb6a1030110f89036c43a1c8fb281c0d
fto0sh <fatimah.s.9@hotmail.com>
2019-10-27 18:30:35 +0300

    day67

---
+++ day67.py
@@ -0,0 +1,11 @@
+x=input("Enter first chart of the name: ")
+y=input("Enter last chart of the name: ")
+print("The First chart of the name: ",x,"\t" ,"The Last chart of the name:",y)
+
+print("--------------------------------------")
+#Day68
+A="Ahmad Ali"
+B=53.44
+print('Dear {},Your current balance is {} $ '.format(A,B))
+
+
